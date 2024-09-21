3f6b86b78978fe10ee8a36a99db2778ed67550d7
axelldev <axellsolisrubio@gmail.com>
2024-09-21 14:14:00 -0600

    last changes

--- components/setps/select-technologies.tsx
+++ components/setps/select-technologies.tsx
@@ -6,36 +6,29 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import { Button } from '../ui/button'
 import AutoSelectTechnologiesButton from './auto-select-technologies-button'
-
-const technologies = [
-  {
-    id: 1,
-    name: 'react',
-    image: '/logos/react.svg',
-  },
-  {
-    id: 2,
-    name: 'vue',
-    image: '/logos/vue.svg',
-  },
-  {
-    id: 3,
-    name: 'angular',
-    image: '/logos/angular.svg',
-  },
-  {
-    id: 4,
-    name: 'laravel',
-    image: '/logos/laravel.png',
-  },
-]
+import { technologiesData } from '@/lib/technologies'
+import { useMemo, useState } from 'react'
 
 export default function SelectTechnologies() {
-  const { isSelected, addTechnology, setAutoAddTechnologies } = useOfferStore(
-    (state) => state,
-  )
-
+  const {
+    selectedTechnologies,
+    isSelected,
+    addTechnology,
+    setAutoAddTechnologies,
+  } = useOfferStore((state) => state)
   const { nextStep } = useStepsStore((state) => state)
+
+  const [displayCount, setDisplayCount] = useState(6)
+  const technologies = useMemo(
+    () => technologiesData.slice(0, displayCount),
+    [displayCount],
+  )
+  const canShowMore = displayCount < technologiesData.length
+
+  const handleShowMore = () => {
+    if (!canShowMore) return
+    setDisplayCount(displayCount + 6)
+  }
 
   const handleClickAutoSelect = () => {
     setAutoAddTechnologies(true)
@@ -43,19 +36,22 @@ export default function SelectTechnologies() {
   }
 
   return (
-    <section className="container">
+    <section className="container mx-auto">
       <div className="flex flex-col items-center">
         <p className="sm:text-2xl md:text-3xl font-bold">
           Selecciona las tecnologias de tu proyecto
         </p>
-        <div className="w-full justify-center grid grid-cols-tech gap-4 mt-4">
+        <div
+          className="justify-center grid place-content-center items-center grid-cols-2 sm:grid-cols-3
+            md:grid-cols-6 mt-4 gap-4"
+        >
           {technologies.map((t) => (
             <div
               key={t.id}
               onClick={() => addTechnology(t)}
               className={cn(
-                `flex flex-col bg-card w-[100px] h-[100px] justify-center items-center rounded-sm
-                p-6 hover:bg-indigo-300/20 cursor-pointer`,
+                `flex flex-col bg-secondary dark:bg-secondary w-[100px] h-[100px] justify-center
+                items-center rounded-sm p-6 hover:bg-indigo-300/20 cursor-pointer`,
                 {
                   'border-2 border-indigo-300 bg-indigo-300/20': isSelected(
                     t.id,
@@ -63,17 +59,34 @@ export default function SelectTechnologies() {
                 },
               )}
             >
-              <Image src={t.image} width={100} height={100} alt="react logo" />
+              <div className="relative size-[80px]">
+                <Image
+                  src={t.image}
+                  fill
+                  alt="react logo"
+                  className="object-contain"
+                />
+              </div>
               <p className="capitalize font-semibold">{t.name}</p>
             </div>
           ))}
         </div>
 
+        {canShowMore && (
+          <div className="flex w-full justify-center mt-6">
+            <Button variant="secondary" onClick={handleShowMore}>
+              Mostar mas
+            </Button>
+          </div>
+        )}
+
         <AutoSelectTechnologiesButton onClick={handleClickAutoSelect} />
 
-        <Button className="mt-8 py-2 w-[300px]" onClick={() => nextStep()}>
-          Continuar
-        </Button>
+        {selectedTechnologies.length > 0 && (
+          <Button className="mt-8 py-2 w-[300px]" onClick={() => nextStep()}>
+            Continuar
+          </Button>
+        )}
       </div>
     </section>
   )
---
+++ lib/technologies.ts
@@ -0,0 +1,160 @@
+export const technologiesData = [
+  {
+    id: 1,
+    name: 'react',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+  },
+  {
+    id: 2,
+    name: 'vue',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+  },
+  {
+    id: 3,
+    name: 'angular',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg',
+  },
+  {
+    id: 4,
+    name: 'laravel',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg',
+  },
+  {
+    id: 5,
+    name: 'nodejs',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+  },
+  {
+    id: 6,
+    name: 'django',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg',
+  },
+  {
+    id: 7,
+    name: 'spring',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/44/Spring_Framework_Logo_2018.svg',
+  },
+  {
+    id: 8,
+    name: 'flutter',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png',
+  },
+  {
+    id: 9,
+    name: 'express',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
+  },
+  {
+    id: 10,
+    name: 'rails',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/6/62/Ruby_On_Rails_Logo.svg',
+  },
+  {
+    id: 11,
+    name: 'python',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+  },
+  {
+    id: 12,
+    name: 'javascript',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+  },
+  {
+    id: 13,
+    name: 'typescript',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+  },
+  {
+    id: 14,
+    name: 'java',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg',
+  },
+  {
+    id: 15,
+    name: 'csharp',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png',
+  },
+  {
+    id: 16,
+    name: 'php',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg',
+  },
+  {
+    id: 17,
+    name: 'ruby',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/73/Ruby_logo.svg',
+  },
+  {
+    id: 18,
+    name: 'golang',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/0/05/Go_Logo_Blue.svg',
+  },
+  {
+    id: 19,
+    name: 'kotlin',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/74/Kotlin_Icon.png',
+  },
+  {
+    id: 20,
+    name: 'swift',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Swift_logo.svg',
+  },
+  {
+    id: 21,
+    name: 'r',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/R_logo.svg',
+  },
+  {
+    id: 22,
+    name: 'rust',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d5/Rust_programming_language_black_logo.svg',
+  },
+  {
+    id: 24,
+    name: 'sass',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg',
+  },
+  {
+    id: 25,
+    name: 'less',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/8/81/LESS_Logo.svg',
+  },
+  {
+    id: 26,
+    name: 'bootstrap',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg',
+  },
+  {
+    id: 27,
+    name: 'jquery',
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/9e/JQuery_logo.svg',
+  },
+  {
+    id: 28,
+    name: 'mysql',
+    image: 'https://upload.wikimedia.org/wikipedia/en/d/dd/MySQL_logo.svg',
+  },
+  {
+    id: 29,
+    name: 'postgresql',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg',
+  },
+]
